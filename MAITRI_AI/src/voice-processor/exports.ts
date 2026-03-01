32b3d52c607db2eeb01ceb02797e7cd2261b9525
premsagar786 <sagarprem2006@gmail.com>
2026-03-01 15:10:05 +0530

    add maitri

---
+++ MAITRI_AI/src/voice-processor/exports.ts
@@ -0,0 +1,9 @@
+/**
+ * Voice Processor Module Exports
+ * 
+ * Public API for the voice processor module
+ */
+
+export { TranscriptionService } from './transcription-service';
+export { handler, TranscriptionEvent, LambdaResponse } from './index';
+export type { TranscriptionServiceConfig } from './transcription-service';
